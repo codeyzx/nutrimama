0c2e2592ed0f136a524bee77a8b17029329a9a5f
codeyzx <yahyatruth@gmail.com>
2024-04-09 11:39:11 +0700

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nutrimama/firebase_options.dart';
 import 'package:nutrimama/src/constants/constants.dart';
 import 'package:nutrimama/src/routes/app_routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
